49ae7717cde2823fef2d965420cef998b6f0c0fb
Dude902 <dude902@gmail.com>
2017-11-17 00:34:23 -0500

    Add files via upload

--- editor.js
+++ editor.js
@@ -236,6 +236,8 @@ function init()
 	setTimeout(draw, 250); //0.25 sec
 	setTimeout(draw, 500); //0.5 sec
 	setTimeout(draw, 1000); //1.0 sec
+	setTimeout(draw, 2000); //1.0 sec
+	setTimeout(draw, 3000); //1.0 sec
 	}
 
 //REDRAW TEXT CANVAS CONTENTS - DRAWN TO VISIBLE CANVAS IN draw()
